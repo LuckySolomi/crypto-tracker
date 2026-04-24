1e967bf27aa923388dcf9ca078e14c8338a4c88d
Solomiia Pitsur <pitsur.solomiia@gmail.com>
2026-04-24 03:10:31 +0300

    Adds router to the project

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,15 @@
 import { Routes } from '@angular/router';
+import { Coins } from './pages/coins/coins';
+import { CoinDetails } from './pages/coin-details/coin-details';
+import { Portfolio } from './pages/portfolio/portfolio';
+import { Wishlist } from './pages/wishlist/wishlist';
+import { NotFound } from './pages/not-found/not-found';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', redirectTo: 'coins', pathMatch: 'full' },
+  { path: 'coins', component: Coins },
+  { path: 'coins/:id', component: CoinDetails },
+  { path: 'portfolio', component: Portfolio },
+  { path: 'wishlist', component: Wishlist },
+  { path: '**', component: NotFound },
+];
