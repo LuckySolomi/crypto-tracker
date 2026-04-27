8fdb7f9647fb38ae057d51115bc2453a7354d0e2
Solomiia Pitsur <pitsur.solomiia@gmail.com>
2026-04-27 03:27:54 +0300

    Edit coin interface

--- src/app/models/coin.interface.ts
+++ src/app/models/coin.interface.ts
@@ -7,4 +7,7 @@ export interface Coin {
   market_cap: number;
   market_cap_rank: number;
   price_change_percentage_24h: number;
+  high_24h: number;
+  low_24h: number;
+  total_volume: number;
 }
