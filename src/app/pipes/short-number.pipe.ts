f65abd509d0be8b4c1b350a4ea5855fd2a3bbda3
Solomiia Pitsur <pitsur.solomiia@gmail.com>
2026-05-02 03:04:07 +0300

    Creates pipe for formating big numbers

---
+++ src/app/pipes/short-number.pipe.ts
@@ -0,0 +1,18 @@
+import { Pipe, PipeTransform } from '@angular/core';
+
+@Pipe({
+  name: 'shortNumber',
+  standalone: true,
+})
+export class ShortNumberPipe implements PipeTransform {
+  transform(value: number): string {
+    if (!value) return '';
+
+    if (value >= 1e12) return (value / 1e12).toFixed(2) + 'T';
+    if (value >= 1e9) return (value / 1e9).toFixed(2) + 'B';
+    if (value >= 1e6) return (value / 1e6).toFixed(2) + 'M';
+    if (value >= 1e3) return (value / 1e3).toFixed(2) + 'K';
+
+    return value.toString();
+  }
+}
